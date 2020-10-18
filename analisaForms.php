5952f158c9feb914c491a4590a4d72353a333af3
gi-armani <giovana_armani@usp.br>
2020-10-18 01:00:22 -0300

    Arquivo de back para formulario

---
+++ analisaForms.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+
+$sexo = mysqli_real_escape_string($conexao, $_POST['sexo']);
+$longevidade = mysqli_real_escape_string($conexao, $_POST['longevidade']);
+$horasSono = mysqli_real_escape_string($conexao, $_POST['horasSono']);
+$cafeDaManha = mysqli_real_escape_string($conexao, $_POST['cafeDaManha']);
+$quantRefeicoes = mysqli_real_escape_string($conexao, $_POST['quantRefeicoes']);
+$frutasVerduras = mysqli_real_escape_string($conexao, $_POST['frutasVerduras']);
+$gorduras = mysqli_real_escape_string($conexao, $_POST['email']);
+$processados = mysqli_real_escape_string($conexao, $_POST['processados']);
+$alcool = mysqli_real_escape_string($conexao, $_POST['alcool']);
+
+$insigth = [
+    "horasSono" => $horasSono,
+    "cafeDaManha" => $cafeDaManha,
+    "quantRefeicoes" => $quantRefeicoes,
+    "frutasVerduras" => $frutasVerduras,
+    "gorduras" => $gorduras,
+    "processados" => $processados,
+    "alcool" => $alcool,
+];
+
+$_SESSION['insigth'] = $insigth;
+header('Location: resultadosForms.php');
+exit();
+
+?>
