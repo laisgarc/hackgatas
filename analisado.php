502a97e076b282e0e73c805960de84da29fce002
gi-armani <giovana_armani@usp.br>
2020-10-18 01:24:29 -0300

    Ajuste nos valores de input do formulario para interpretacao

---
+++ analisado.php
@@ -0,0 +1,46 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <link rel="stylesheet" href="https://indestructibletype.com/fonts/Jost.css" type="text/css" charset="utf-8" />
+        <meta charset="UTF-8">
+        <title>Avalie seus hábitos</title> 
+
+        <!--Importando bootstrap-->
+         <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> 
+        <!--Acabei de importar bootstrap-->
+
+        <link rel="stylesheet" href="CSS/formulario.css">
+    </head>
+
+    <body>
+        <div class='container'>
+            <br><br>
+            <h1 class='text-center'>Avalie seus hábitos e entenda<br> o que eles significam</h1></br>
+            <p>O algoritmo de cálculo de respostas será validado por uma nutricionista.</p>
+            <img class='logo' src='imgs/logo.png'>
+            <br><br>
+        
+            <div class="card mx-auto form-card">
+                <div class="card-body">
+                    
+                    
+                </div>
+            </div>
+
+
+        </div>
+
+    </body>
+</html>
